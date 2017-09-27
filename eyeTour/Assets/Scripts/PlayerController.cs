026de9466ef8ef2a99183826eaf62a1aa55d176d
Timothy Beck <timothy.beck@uoit.net>
2017-09-26 20:39:11 -0400

    In MainMenu: Added a button to go to the level. In Level: Added walls and moved a few things around, edited materials and movement script.

--- eyeTour/Assets/Scripts/PlayerController.cs
+++ eyeTour/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Update()
+    public float speed = 3.0f;
+    public float turnspeed = 150.0f;
+
+    private Rigidbody rb;
+    private void Start()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        rb = GetComponent<Rigidbody>();
+
+    }
+    /*void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnspeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, -z);
+    }*/
+    void FixedUpdate()
+    {
+        var x = Input.GetAxis("Horizontal") * turnspeed / 60.0f;
+        var z = Input.GetAxis("Vertical") * speed / 60.0f;
+
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, -z);
