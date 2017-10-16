2fbe55c12a0e79ddfadaa6539af89fdc6193482f
Russ <russellsng@gmail.com>
2017-10-16 14:01:26 -0400

    + Added Smooth rotation to the EyeTracker

--- eyeTour/Assets/Scripts/PlayerController.cs
+++ eyeTour/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 3.0f;
     public float turnspeed = 150.0f;
+    private float curTurnSpeed = 0.0f;
 
     private Rigidbody rb;
     private void Start()
@@ -41,33 +42,32 @@ public class PlayerController : MonoBehaviour
         Vector2 userGaze = TobiiAPI.GetGazePoint().Screen;
         smoothedView = Vector2.Lerp(smoothedView, userGaze, 0.5f); //Smooths gaze points 
 
+        curTurnSpeed = 0.0f;
+
         Debug.Log(userGaze); //Outputs where the user is looking in x,y px to console
 
-        if (userGaze.x <= Screen.width / 8)
+        if (userGaze.x <= Screen.width / 8 || userGaze.x >= Screen.width)
         {
+            curTurnSpeed += turnspeed / 5.0f;
 
-            transform.Rotate(0, -.5f * turnspeed / 60.0f, 0);
+            if (curTurnSpeed > turnspeed)
+                curTurnSpeed = turnspeed;
+
+            float angleFactor = (userGaze.x <= Screen.width / 8) ? -1.0f : 1.0f;
+
+            Quaternion r1 = Quaternion.AngleAxis(angleFactor * curTurnSpeed * Time.deltaTime, Vector3.up);
+            //transform.Rotate(0, -curTurnSpeed * Time.deltaTime, 0); // Using the transform.rotate instead of quaternions
+
+            transform.rotation = r1 * transform.rotation;
             //var desiredRotationQ = Quaternion.Euler(0, transform.rotation.y + desiredRotation, 0);
+            //var desiredRotationQ = Quaternion.AngleAxis(, Vector3.up Time.deltaTime * speed);
 
             //transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotationQ, Time.deltaTime * damping);
 
             // transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotationQ , Time.time * damping);
         }
-
-        else if (userGaze.x >= Screen.width)
-        {
-            transform.Rotate(0, .5f * turnspeed / 60.0f, 0);
-        }
-
-        //else if (userGaze.x <= 100)
-        // {
-        //     transform.Rotate(0, userGaze.x, 0);
-        // }
-
-        // else if (userGaze.x <= 100)
-        // {
-        //     transform.Rotate(0, userGaze.x, 0);
-        // }
+        else
+            curTurnSpeed = 0.0f;
 
         //Debug.Log("test");
         //Debug.Log(smoothedView);
